222e779a0716616f46b782b248ef4b6e3f41b65f
Shakib448 <shakiba448@gmail.com>
2020-09-30 06:55:13 +0600

    Product key updated

--- server.js
+++ server.js
@@ -5,18 +5,37 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qebvf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const PORT = 5000;
 
-const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qebvf.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const collection = client.db("emaJohnStore").collection("products");
-  console.log("Data base connected");
-  client.close();
+  const productsCollection = client.db("emaJohnStore").collection("products");
+
+  app.post("/addProduct", (req, res) => {
+    const products = req.body;
+    productsCollection.insertMany(products).then((result) => {
+      console.log(result.insertedCount);
+      res.send(result.insertedCount);
+    });
+  });
+
+  app.get("/products", (req, res) => {
+    productsCollection.find({}).toArray((err, doc) => {
+      res.send(doc);
+    });
+  });
+
+  app.get("/product/:key", (req, res) => {
+    productsCollection.find({ key: req.params.key }).toArray((err, doc) => {
+      res.send(doc[0]);
+    });
+  });
 });
 
 app.use(cors());
