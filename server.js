40a93a492aa61af34a325938dc8f70ab8b5d4094
Shakib448 <shakiba448@gmail.com>
2020-10-01 07:55:42 +0600

    Order const added

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const port = 5000;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -48,17 +51,15 @@ client.connect((err) => {
   });
 
   app.post("/addOrder", (req, res) => {
+    const order = req.body;
     ordersCollection.insertOne(order).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
 });
 
-app.use(cors());
-app.use(bodyParser.json());
-
 app.get("/", (req, res) => {
   res.send("Hello the db it's working in the heroku site");
 });
 
-app.listen(process.env.PORT || port);
+app.listen(port);
